88373bb74e407b5eb99db0c33075817cba4272bb
lakb248 <lakb248@gmail.com>
2017-01-15 23:05:32 +0800

    add task card

---
+++ app/components/task.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="task-card">
+        <div class="task-card--content">
+            {{task.content}}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['task'],
+    data() {
+        return {};
+    }
+};
+</script>
+
+<style lang="sass">
+    @import "../styles/theme.scss";
+    .task-card {
+        box-shadow: $box-shadow;
+        padding: 10px;
+        &--content {
+            font-size: 14px;
+        }
+    }
+</style>
