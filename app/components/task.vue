83dfd19b3cb32dc5e94602b5a74c43c782c5a0dc
lakb248 <lakb248@gmail.com>
2017-01-19 09:57:30 +0800

    update task component, add avatar of members

--- app/components/task.vue
+++ app/components/task.vue
@@ -3,6 +3,11 @@
         <div class="task-card--content">
             {{task.content}}
         </div>
+        <div class="task-card--member">
+            <span class="member-avatar" v-for="member in task.involveMembers"
+                :style="{'background-image': 'url(' + member.avatarUrl + ')'}"
+            ></span>
+        </div>
     </div>
 </template>
 
@@ -20,8 +25,23 @@ export default {
     .task-card {
         box-shadow: $box-shadow;
         padding: 10px;
+        overflow: hidden;
         &--content {
+            float: left;
             font-size: 14px;
         }
+        &--member {
+            float: right;
+        }
+    }
+    .member-avatar {
+        display: inline-block;
+        width: 20px;
+        height: 20px;
+        border-radius: 50%;
+        background-repeat: no-repeat;
+        background-position: center;
+        background-size: cover;
+        margin-right: 5px;
     }
 </style>
