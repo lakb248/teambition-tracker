a7e436ff8a6d4f3710a764c016bcf1ae50eb6fe0
lakb248 <lakb248@gmail.com>
2017-01-22 10:35:20 +0800

    update util

--- app/utils/util.js
+++ app/utils/util.js
@@ -1,9 +1,33 @@
 export default {
+    isArray(obj) {
+        if (typeof obj === 'object') {
+            var toStr = Object.prototype.toString;
+            if (toStr.apply(obj).toLowerCase() === '[object array]') {
+                return true;
+            }
+            return false;
+        }
+        return false;
+    },
     arrayToObject(array, key) {
         array = array || [];
         let obj = {};
         array.forEach(item => {
-            obj[item[key]] = item;
+            let objKey = item[key];
+            if (obj[objKey]) {
+                // objKey is exist
+                if (this.isArray(obj[objKey])) {
+                    // obj[objKey] is array
+                    obj[objKey].push(item);
+                } else {
+                    let temp = obj[objKey];
+                    obj[objKey] = [];
+                    obj[objKey].push(temp);
+                    obj[objKey].push(item);
+                }
+            } else {
+                obj[objKey] = item;
+            }
         });
         return obj;
     }
