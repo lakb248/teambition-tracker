436c4051712139b070a0926bd68b4f4c6f86e4a6
lakb248 <lakb248@gmail.com>
2017-02-14 00:34:13 +0800

    update unti test

--- test/utils/calendar-util.spec.js
+++ test/utils/calendar-util.spec.js
@@ -3,7 +3,17 @@ import Calendar from '../../app/utils/calendar-util.js';
 
 // isLeap
 describe('utils/calendar-util.js === isLeap', () => {
-    it('should 2012 be a leap year', () => {
+    it('should 2011 not be a leap year', () => {
         expect(Calendar.isLeap(2011)).toBe(false);
+    });
+    it('should 2012 be a leap year', () => {
+        expect(Calendar.isLeap(2012)).toBe(true);
+    });
+});
+
+// getMaxDayOfMonth
+describe('utils/calendar-util.js === getMaxDayOfMonth', () => {
+    it('should have 28 days in 2017.02', () => {
+        expect(Calendar.getMaxDayOfMonth(2017, 2)).toBe(28);
     });
 });
