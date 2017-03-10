333b7cc795bcec2dcb7a29b16435c880b40db244
lakb248 <lakb248@gmail.com>
2017-03-10 17:42:01 +0800

    update cache by simple map

--- app/utils/cache.js
+++ app/utils/cache.js
@@ -1,10 +1,1 @@
-import {clone} from './util';
-let cache = {};
-export default {
-    get(key) {
-        return clone(cache[key]);
-    },
-    set(key, value) {
-        cache[key] = value;
-    }
-};
+export default new Map();
